dcda242a0d1ce349e4e194c920d7f74c43e31137
qincasin <qincasin@163.com>
2023-05-07 22:31:38 +0800

    feat: init

---
+++ mini-spring/mini-spring/src/main/java/com/qjx/mini/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.qjx.mini;
+
+/**
+ * <Description>
+ *
+ * @author qinjiaxing on 2023/5/7
+ * @author <others>
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println(11111);
+    }
+
+}
---
+++ mini-spring/mini-spring/src/main/java/com/qjx/mini/test/AService.java
@@ -0,0 +1,11 @@
+package com.qjx.mini.test;
+
+/**
+ * <Description>
+ *
+ * @author qinjiaxing on 2023/5/7
+ * @author <others>
+ */
+public class AService {
+
+}
